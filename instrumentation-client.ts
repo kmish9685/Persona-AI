421ff4fedb71eb66260fed0fa517e40f92d9b856
kmish96980-8944 <pro9685646668@gmail.com>
2026-02-07 08:47:32 +0530

    qwq

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,11 +2,9 @@
 
 import posthog from 'posthog-js';
 
-export function register() {
-    if (typeof window !== 'undefined') {
-        posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-            api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-            person_profiles: 'identified_only',
-        });
-    }
+if (typeof window !== 'undefined') {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+        person_profiles: 'identified_only',
+    });
 }
